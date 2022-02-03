6a62361451934bbf95de5b55d56ece13cf7d95a2
Thomas Baudry <2132465@cegeprdl.ca>
2022-02-03 13:57:47 -0500

    Ajoutez des fichiers projet.

---
+++ TPWEB/Controllers/CegepController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using GestionCegepWeb.Logics.Controleurs;
+
+/// <summary>
+/// Namespace pour les controleurs de vue.
+/// </summary>
+namespace GestionCegepWeb.Controllers
+{
+    /// <summary>
+    /// Classe représentant le controleur de vue des Cégeps.
+    /// </summary>
+    public class CegepController : Controller
+    {
+        /// <summary>
+        /// Méthode de service appelé lors de l'action Index.
+        /// Rôles de l'action : 
+        ///   -Afficher la liste des Cégeps.
+        /// </summary>
+        /// <returns>ActionResult suite aux traitements des données.</returns>
+        [Route("")]
+        [Route("Cegep")]
+        [Route("Cegep/Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                //Préparation des données pour la vue...
+                ViewBag.ListeCegeps = CegepControleur.Instance.ObtenirListeCegep().ToArray();
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+            
+            //Retour de la vue...
+            return View();
+        }
+    }
+}
