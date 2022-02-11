1b88435d0a25c4fc1e340648de4a7f06a2df2267
Thomas Baudry <2132465@cegeprdl.ca>
2022-02-11 09:12:07 -0500

    Ajout controleur departement et Enseignant.

---
+++ TPWEB/Controllers/DepartementController.cs
@@ -0,0 +1,32 @@
+﻿using GestionCegepWeb.Logics.Controleurs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GestionCegepWeb.Controllers
+{
+    public class DepartementController : Controller
+    {
+        [Route("Departement")]
+        [Route("Departement/Index")]
+        [HttpGet]
+        public IActionResult Index([FromQuery] string nomCegep)
+        {
+            try
+            {
+                if(nomCegep == null)
+                {
+                    nomCegep = CegepControleur.Instance.ObtenirListeCegep()[0].Nom;
+                }
+                //Préparation des données pour la vue...
+                ViewBag.ListeDepartement = CegepControleur.Instance.ObtenirListeDepartement(nomCegep).ToArray();
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+
+            //Retour de la vue...
+            return View();
+        }
+    }
+}
---
+++ TPWEB/Controllers/EnseignantController.cs
@@ -0,0 +1,47 @@
+﻿using GestionCegepWeb.Logics.Controleurs;
+using GestionCegepWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TPWEB.Controllers
+{
+    public class EnseignantController : Controller
+    {
+        [Route("Enseignant")]
+        [Route("Enseignant/Index")]
+        [HttpGet]
+        public IActionResult Index([FromQuery] string nomCegep, [FromQuery] string nomDepartement)
+        {
+            try
+            {
+                if (nomCegep == null)
+                {
+                    nomCegep = CegepControleur.Instance.ObtenirListeCegep()[0].Nom;
+                }
+                bool ok = false;
+                foreach( DepartementDTO dep in CegepControleur.Instance.ObtenirListeDepartement(nomCegep))
+                {
+                    if(dep.Nom == nomDepartement)
+                    {
+                        ok  = true;
+                    }
+                }
+                if (nomDepartement == null || ok == false)
+                {
+                    nomDepartement = CegepControleur.Instance.ObtenirListeDepartement(nomCegep)[0].Nom;
+                }
+                ViewBag.NomCegep = nomCegep;
+                ViewBag.NowDepartement = nomDepartement;
+                //Préparation des données pour la vue...
+                ViewBag.ListeEnseignants = CegepControleur.Instance.ObtenirListeEnseignant(nomCegep, nomDepartement).ToArray();
+            }
+            catch (Exception e)
+            {
+                ViewBag.MessageErreur = e.Message;
+            }
+
+            //Retour de la vue...
+            return View();
+        }
+    }
+}
